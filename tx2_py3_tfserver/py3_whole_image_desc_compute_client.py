b560a28368ce04be181930d3558fe6b0d72ae68a
Manohar Kuse <mpkuse@connect.ust.hk>
2019-06-10 17:00:09 +0800

    a working tensorflow server on tx2

--- tx2_py3_tfserver/py3_whole_image_desc_compute_client.py
+++ tx2_py3_tfserver/py3_whole_image_desc_compute_client.py
@@ -14,12 +14,15 @@ from sensor_msgs.msg import Image
 import rospkg
 THIS_PKG_BASE_PATH = rospkg.RosPack().get_path('tx2_whole_image_desc_server')
 
+print ('Attempt connecting to server')
 rospy.wait_for_service( 'whole_image_descriptor_compute' )
+print ('Connection successful!!')
 try:
     res = rospy.ServiceProxy( 'whole_image_descriptor_compute', WholeImageDescriptorCompute )
 
     # X = np.zeros( (100, 100), dtype=np.uint8 )
     X = cv2.resize( cv2.imread( THIS_PKG_BASE_PATH+'/resources/lena_color.jpg' ), (752,480) )
+    # X = cv2.resize( cv2.imread( THIS_PKG_BASE_PATH+'/resources/lena_color.jpg' ), (7,5) )
     print( 'X.shape=', X.shape )
     i = CvBridge().cv2_to_imgmsg( X )
     u = res( i, 23  )
--- tx2_py3_tfserver/py3_whole_image_desc_server.py
+++ tx2_py3_tfserver/py3_whole_image_desc_server.py
@@ -1,16 +1,64 @@
 #!/usr/bin/python3
+# Tensorflow
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
+# OpenCV
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+# Misc Python packages
+import numpy as np
+import os
+import time
+
+# ROS
 import rospy
+
+# Pkg msg definations
+from tx2_whole_image_desc_server.srv import WholeImageDescriptorCompute, WholeImageDescriptorComputeResponse
 
 from TerminalColors import bcolors
 tcol = bcolors()
 
-import tensorflow as tf
-from tensorflow.keras import backend as K
-import numpy as np
+def imgmsg_to_cv2( msg ):
+    """ Drop in replacement for cv_bridge's imgmsg_to_cv2.
+    When I used cv_bridge with python3, I got the following:
+    ```
+    [ERROR] [1560146920.764209]: Error processing request: dynamic module does not define module export function (PyInit_cv_bridge_boost)
+    ```
+    This meant that cv_bridge was only compiled with python2 support. Couldnt get it to work, so
+    just coded up a simple imgmsg_to_cv2
 
-import os
 
-from tx2_whole_image_desc_server.srv import WholeImageDescriptorCompute, WholeImageDescriptorComputeResponse
+    The image msg looks something like:
+    handle_req
+    header:
+        seq: 0
+        stamp:
+            secs: 0
+            nsecs:         0
+        frame_id: ''
+    height: 5
+    width: 7
+    encoding: "8UC3"
+    is_bigendian: 0
+    step: 21
+    data: [111, 135, 230, 82, 76, 191, 91, 91, 208, 110, 105, 182, 83, 73, 183, 95, 110, 213, 54, 14, 85, 106, 137, 229, 72, 64, 177, 103, 106, 198, 142, 148, 220, 131, 136, 197, 173, 194, 221, 124, 137, 211, 110, 136, 227, 71, 64, 182, 110, 61, 121, 74, 61, 179, 105, 93, 190, 57, 14, 87, 126, 133, 201, 114, 134, 224, 81, 36, 96, 64, 25, 93, 95, 98, 213, 59, 16, 85, 120, 134, 201, 157, 199, 238, 91, 110, 216, 81, 35, 98, 57, 17, 99, 96, 104, 218, 143, 164, 222, 114, 118, 185, 66, 38, 134]
+    """
+
+    assert msg.encoding == "8UC3" or msg.encoding == "8UC1", "Expecting the msg to have encoding as 8UC3 or 8UC1, received"+ str( msg.encoding )
+    if msg.encoding == "8UC3":
+        X = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+        return X
+
+    if msg.encoding == "8UC1":
+        X = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width)
+        return X
+
+
+
+
 
 # This was copied from the training code's CustomNets.py
 class NetVLADLayer( tf.keras.layers.Layer ):
@@ -93,15 +141,19 @@ class HDF5ModelImageDescriptor:
     you need to specify the full path (keras model file).
     """
     def __init__(self, kerasmodel_file, im_rows=600, im_cols=960, im_chnls=3):
+        # return
         ## Build net
         # from keras.backend.tensorflow_backend import set_session
         # tf.set_random_seed(42)
-        # config = tf.ConfigProto()
+        config = tf.ConfigProto()
         # config.gpu_options.per_process_gpu_memory_fraction = 0.2
-        # config.gpu_options.visible_device_list = "0"
-        # tf.keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
-        #TODO set learning phase(0)
+        config.gpu_options.visible_device_list = "0"
+        config.intra_op_parallelism_threads=1
+        tf.keras.backend.set_session(tf.Session(config=config))
         tf.keras.backend.set_learning_phase(0)
+
+        self.sess = tf.keras.backend.get_session()
+
 
         # Blackbox 4
         # self.im_rows = 512
@@ -165,69 +217,95 @@ class HDF5ModelImageDescriptor:
         print( '\tdtype=', tmp_zer_out.dtype )
         print( '-----' )
 
+        print ( 'tmp_zer_out=', tmp_zer_out )
 
-    def handle_req( self, req ):
+
+    def handle_req1( self, req ):
         print ('handle_req')
+        print( req.ima )
+        # cv_image = CvBridge().imgmsg_to_cv2( req.ima )
+        cv_image = imgmsg_to_cv2( req.ima )
+        # cv2.imshow( 'win', cv_image.astype('uint8') )
+        # cv2.waitKey(0)
+        # cv2.imwrite( '/tmp/x.jpg', cv_image )
+
+        i__image = np.expand_dims( cv_image.astype('float32'), 0 )
+        print ('i__image.shape=', i__image.shape )
+
+
+        with self.sess.as_default():
+            with self.sess.graph.as_default():
+                u = self.model.predict( i__image )
+        # u = self.model.predict( i__image )
+        print( 'u=', u )
+
+
         result = WholeImageDescriptorComputeResponse()
         result.desc = [ 2122., 46.2 ]
-        result.model_type = self.model_type
+        result.model_type = 'test' #self.model_type
         return result
 
-    #
-    # def handle_req( self, req ):
-    #     """ The received image from CV bridge has to be [0,255]. In function makes it to
-    #     intensity range [-0.5 to 0.5]
-    #     """
-    #     ## Get Image out of req
-    #     cv_image = CvBridge().imgmsg_to_cv2( req.ima )
-    #     print '[HDF5ModelImageDescriptor Handle Request] cv_image.shape', cv_image.shape, '\ta=', req.a, '\tt=', req.ima.header.stamp
-    #     if len(cv_image.shape)==2:
-    #         # print 'Input dimensions are NxM but I am expecting it to be NxMxC, so np.expand_dims'
-    #         cv_image = np.expand_dims( cv_image, -1 )
-    #     elif len( cv_image.shape )==3:
-    #         pass
-    #     else:
-    #         assert( False )
-    #
-    #
-    #
-    #
-    #     assert (cv_image.shape[0] == self.im_rows and
-    #             cv_image.shape[1] == self.im_cols and
-    #             cv_image.shape[2] == self.im_chnls),\
-    #             "\n[whole_image_descriptor_compute_server] Input shape of the image \
-    #             does not match with the allocated GPU memory. Expecting an input image of \
-    #             size %dx%dx%d, but received : %s" %(self.im_rows, self.im_cols, self.im_chnls, str(cv_image.shape) )
-    #
-    #     # cv2.imshow( 'whole_image_descriptor_compute_server:imshow', cv_image.astype('uint8') )
-    #     # cv2.waitKey(10)
-    #     # cv2.imwrite( '/app/tmp/%s.jpg' %( str(req.ima.header.stamp) ), cv_image )
-    #
-    #     ## Compute Descriptor
-    #     start_time = time.time()
-    #     # i__image = np.expand_dims( cv_image.astype('float32'), 0 )
-    #     # i__image = (np.expand_dims( cv_image.astype('float32'), 0 ) - 128.)/255. [-0.5,0.5]
-    #     i__image = (np.expand_dims( cv_image.astype('float32'), 0 ) - 128.)*2.0/255. #[-1,1]
-    #
-    #     u = self.model.predict( i__image )
-    #
-    #     print tcol.HEADER, 'Descriptor Computed in %4.4fms' %( 1000. *(time.time() - start_time) ), tcol.ENDC
-    #     print '\tinput_image.shape=', cv_image.shape,
-    #     print '\tinput_image dtype=', cv_image.dtype
-    #     print tcol.OKBLUE, '\tinput image (to neuralnet) minmax=', np.min( i__image ), np.max( i__image ), tcol.ENDC
-    #     print '\tdesc.shape=', u.shape,
-    #     print '\tdesc minmax=', np.min( u ), np.max( u ),
-    #     print '\tnorm=', np.linalg.norm(u[0])
-    #     print '\tmodel_type=', self.model_type
-    #
-    #
-    #
-    #     ## Populate output message
-    #     result = WholeImageDescriptorComputeResponse()
-    #     # result.desc = [ cv_image.shape[0], cv_image.shape[1] ]
-    #     result.desc = u[0,:]
-    #     result.model_type = self.model_type
-    #     return result
+
+
+    def handle_req( self, req ):
+        """ The received image from CV bridge has to be [0,255]. In function makes it to
+        intensity range [-1 to 1]
+        """
+
+        # import code
+        # code.interact( local=locals() )
+        ## Get Image out of req
+        # cv_image = CvBridge().imgmsg_to_cv2( req.ima )
+        cv_image = imgmsg_to_cv2( req.ima )
+        print( '[HDF5ModelImageDescriptor Handle Request] cv_image.shape', cv_image.shape, '\ta=', req.a, '\tt=', req.ima.header.stamp )
+        if len(cv_image.shape)==2:
+            # print 'Input dimensions are NxM but I am expecting it to be NxMxC, so np.expand_dims'
+            cv_image = np.expand_dims( cv_image, -1 )
+        elif len( cv_image.shape )==3:
+            pass
+        else:
+            assert False
+
+
+
+
+        assert (cv_image.shape[0] == self.im_rows and cv_image.shape[1] == self.im_cols and cv_image.shape[2] == self.im_chnls) , \
+                "\n[whole_image_descriptor_compute_server] Input shape of the image \
+                does not match with the allocated GPU memory. Expecting an input image of \
+                size %dx%dx%d, but received : %s" %(self.im_rows, self.im_cols, self.im_chnls, str(cv_image.shape) )
+
+        # cv2.imshow( 'whole_image_descriptor_compute_server:imshow', cv_image.astype('uint8') )
+        # cv2.waitKey(10)
+        # cv2.imwrite( '/app/tmp/%s.jpg' %( str(req.ima.header.stamp) ), cv_image )
+
+        ## Compute Descriptor
+        start_time = time.time()
+        # i__image = np.expand_dims( cv_image.astype('float32'), 0 )
+        # i__image = (np.expand_dims( cv_image.astype('float32'), 0 ) - 128.)/255. [-0.5,0.5]
+        i__image = (np.expand_dims( cv_image.astype('float32'), 0 ) - 128.)*2.0/255. #[-1,1]
+
+        # u = self.model.predict( i__image )
+        with self.sess.as_default():
+            with self.sess.graph.as_default():
+                u = self.model.predict( i__image )
+
+        print( tcol.HEADER, 'Descriptor Computed in %4.4fms' %( 1000. *(time.time() - start_time) ), tcol.ENDC )
+        print( '\tinput_image.shape=', cv_image.shape, )
+        print( '\tinput_image dtype=', cv_image.dtype )
+        print( tcol.OKBLUE, '\tinput image (to neuralnet) minmax=', np.min( i__image ), np.max( i__image ), tcol.ENDC )
+        print( '\tdesc.shape=', u.shape, )
+        print( '\tdesc minmax=', np.min( u ), np.max( u ), )
+        print( '\tnorm=', np.linalg.norm(u[0]) )
+        print( '\tmodel_type=', self.model_type )
+
+
+
+        ## Populate output message
+        result = WholeImageDescriptorComputeResponse()
+        # result.desc = [ cv_image.shape[0], cv_image.shape[1] ]
+        result.desc = u[0,:]
+        result.model_type = self.model_type
+        return result
 
 if __name__ == '__main__':
     rospy.init_node( 'whole_image_descriptor_compute_server' )
